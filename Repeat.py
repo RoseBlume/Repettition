1f16fa0fb7e3670120b69e942e21f5c8ec5df93c
RoseBlume <92864596+RoseBlume@users.noreply.github.com>
2021-11-04 14:26:12 -0400

    Update Repeat.py

--- Repeat.py
+++ Repeat.py
@@ -1,8 +1,8 @@
-from Repeat import R
-#Example of use
-c = 0
-for i in range(len(R.repeat(5))):
-    c += 1
-    c = str(c)
-    print("Count is " + c)
-    c = int(c)
+class R:
+    def repeat(a):
+        b = 0
+        li = []
+        while a > b:
+            li.append(0)
+            b += 1
+        return(li)
